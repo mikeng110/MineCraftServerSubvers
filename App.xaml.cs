408b187c32eb96d14bc5ca0e17e0497b925fa1ce
Mikael Englund <mikael.englund110@gmail.com>
2021-11-05 22:34:28 +0100

    Project in dotnet core
    Decided to use dotnet core, for cross platform and library support

---
+++ App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using LibGit2Sharp;
+using System.Diagnostics;
+
+namespace MineCraftServerSubvers
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //todo: Research best place to put logic in wpf apps
+        public App()
+        {
+            using (var repo = new Repository(@"..\..\..\..\MinecraftServer"))
+            {
+                var branches = repo.Branches;
+                foreach(var b in branches)
+                {
+                    Trace.WriteLine(b.FriendlyName);
+                    
+                }
+
+                foreach(var commit in repo.Commits)
+                {
+                    Trace.WriteLine(
+                        $"{commit.Id.ToString().Substring(0, 7)} " +
+                        $"{commit.Author.When.ToLocalTime()} " +
+                        $"{commit.MessageShort} " +
+                        $"{commit.Author.Name}");
+                    
+                }
+                repo.Network.Fetch("origin", null); //cant be null require credentials 
+
+                
+            }
+        }
+
+    }
+}
